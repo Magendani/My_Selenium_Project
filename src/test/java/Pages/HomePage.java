1098fabb14aac8e7c14d4c23f134c49b14937ca6
Magendani <magendani01@gmail.com>
2025-10-26 19:47:28 +0200

    created Log In page, with methods: verifyLogInPageIsDisplayed() enterEmailAddress() enterPassword() clickLogInButton()

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -11,16 +11,17 @@ public class HomePage {
     @FindBy(id = "overview-hero")
     WebElement homepageHeading_id;
 
+    @FindBy(id = "nav-btn-practice")
+    WebElement learningMaterialsButton_id;
+
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void verifyHomePageIsDisplayed() {homepageHeading_id.isDisplayed();
-    }
+    public void verifyHomePageIsDisplayed() {homepageHeading_id.isDisplayed();}
+    public void clickLearningMaterialsButton() {learningMaterialsButton_id.click();}
 
-        @FindBy(id = "nav-btn-practice")
-        WebElement learningMaterialsButton_id;
 
-        public void clickLearningMaterialsButton() {learningMaterialsButton_id.click();}
+
 
 }
